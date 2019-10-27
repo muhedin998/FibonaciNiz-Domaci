6f665944bf8273a6763c223ec7d1d2a095be5b02
muhedin <muhedin1998@gmail.com>
2019-10-27 22:40:40 +0100

    Add project files.

---
+++ fibonachi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a=0;
+            long b = 1;
+            long c;
+            Console.WriteLine("Do kog broja zelite da idete: ");
+            int brojac = int.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+            while (brojac >=0)
+            {
+              
+                    Console.WriteLine(b);
+                    c = a + b;
+                    a = b;
+                    b = c;
+             
+                brojac--;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
